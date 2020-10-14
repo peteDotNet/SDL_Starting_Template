24ce448f254ced4f129915c188771368485c83ff
Peter Morton <petermorton@live.co.uk>
2020-10-14 18:01:44 +0100

    Add project files.

---
+++ SDL_Starting_Template/Game.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include<SDL.h>
+#include <iostream>
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos,int width, int height, bool fullscreen);
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool running() { return isRunning; }
+
+private:
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	int count = 0;
+};
